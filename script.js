d0d6f614f6735215c31ca1813a404e0910021d9f
Sahil Dhingra <sahildhingra221@gmail.com>
2025-10-06 17:35:42 +0530

    update meta for seo and redirect

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Redirect to sahildhingra.in
-// if (window.location.hostname !== "sahildhingra.in") {
-//   window.location.href = "https://sahildhingra.in";
-// }
+ if (window.location.hostname !== "sahildhingra.com") {
+   window.location.href = "https://sahildhingra.com";
+ }
 
 // Header Sticky
 let lastScrollPos = 0;
